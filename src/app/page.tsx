017a454711c3b384959fc0100dcd65d26813164f
Leon <LAnneken09@gmail.com>
2026-07-10 14:18:02 +0200

    Added a landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,92 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { DottedGlowBackground } from "@/components/ui/dotted-glow-background";
+import { Metadata } from "next";
+import Link from "next/link";
 
+export const metadata: Metadata = {
+  title: "Scrim POVs",
+  description:
+    "Watch scrim VODs with ease. Switch between different POVs and highlight specific points in time",
+  authors: [{ name: "LeonTM", url: "https://leontm.me" }],
+  keywords: [
+    "scrims",
+    "valorant",
+    "cs2",
+    "counter strike",
+    "vods",
+    "vod",
+    "youtube",
+    "povs",
+    "multiple videos",
+  ],
+};
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div className="flex min-h-screen max-w-screen flex-col gap-4">
+      <main className="flex h-screen w-full flex-col items-center justify-center">
+        <DottedGlowBackground
+          className="pointer-events-none mask-radial-to-90% mask-radial-at-center opacity-20 dark:opacity-100"
+          opacity={1}
+          gap={10}
+          radius={1.6}
+          colorLightVar="--color-neutral-500"
+          glowColorLightVar="--color-neutral-600"
+          colorDarkVar="--color-neutral-500"
+          glowColorDarkVar="--color-sky-800"
+          backgroundOpacity={0}
+          speedMin={0.3}
+          speedMax={1.6}
+          speedScale={1}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+        <div className="absolute top-0 left-0 flex w-full flex-row items-center justify-end gap-2 p-4">
+          <Link href={"/sessions/local"}>
+            <Button variant={"secondary"}>Local session</Button>
+          </Link>
+          <Link href={"/sessions"}>
+            <Button variant={"default"} disabled>
+              Start
+            </Button>
+          </Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex flex-col gap-2">
+          <h1 className="text-center text-2xl md:text-4xl">
+            <span className="text-primary font-bold">Scrim POVs</span> - VODs
+            made easy
+          </h1>
+          <p className="text-muted-foreground">
+            Add multiple YouTube videos to a session and watch different POVs
+            alongside each other.
+          </p>
+          <div className="flex flex-row items-center justify-center gap-2">
+            <Link href={"/sessions"}>
+              <Button>create a session</Button>
+            </Link>
+          </div>
         </div>
       </main>
+      <div className="flex w-full flex-row items-center justify-center">
+        <div className="flex w-3/4 flex-row items-center justify-center gap-8 py-8">
+          <p className="text-muted-foreground text-xs">
+            Made with {"<3"} by{" "}
+            <Link
+              href={"https://github.com/leontm-dev"}
+              target="_blank"
+              className="text-primary underline"
+            >
+              LeonTM
+            </Link>
+          </p>
+          <p className="text-muted-foreground text-xs">
+            Source code available on{" "}
+            <Link
+              href={"https://github.com/leontm-dev/scrim-povs"}
+              className="text-primary underline"
+            >
+              GitHub
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
